b25ca9ee137a8c1fad6c90fd5e67ec6fdcf57f0f
Karl-Philipp Richter <krichter722@aol.de>
2018-01-30 13:43:11 +0100

    demonstration of API change proposed at https://stackoverflow.com/questions/44604886/how-specify-a-jpa-criteria-api-with-member-in-and-a-many-to-many-relationship

--- src/main/java/richtercloud/jpa/criteria/api/metamodel/member/of/NewMain.java
+++ src/main/java/richtercloud/jpa/criteria/api/metamodel/member/of/NewMain.java
@@ -5,6 +5,7 @@
  */
 package richtercloud.jpa.criteria.api.metamodel.member.of;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.PluralAttribute;
 import javax.persistence.metamodel.SetAttribute;
@@ -26,6 +28,11 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        main0();
+        main1();
+    }
+
+    private static void main0() {
         EntityManager em = null;
         AUser user = null;
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -39,5 +46,56 @@ public class NewMain {
         TypedQuery<ConcreteEntity> query = em.createQuery(criteria);
         List<ConcreteEntity> result = query.getResultList();
     }
-    
+
+    private static void main1() {
+        MyEntityManager em = null;
+        AUser user = null;
+        MyCriteriaBuilder cb = em.getCriteriaBuilder();
+        MyCriteriaQuery<ConcreteEntity> criteria = cb.createQuery(ConcreteEntity.class);
+        MyRoot<ConcreteEntity> concreteRoot = criteria.from(ConcreteEntity.class);
+        criteria.select(concreteRoot);
+        SetAttribute<AbstractEntity, AUser> b = ConcreteEntity_.createdBy;
+        PluralAttribute<AbstractEntity, Set<AUser>, AUser> d = b;
+        MyExpression<Set<AUser>> c = concreteRoot.get(d);
+        criteria.where(cb.isNotMember(user, c));
+        MyTypedQuery<ConcreteEntity> query = em.createQuery(criteria);
+        List<ConcreteEntity> result = query.getResultList();
+    }
+
+    private interface MyEntityManager {
+        MyCriteriaBuilder getCriteriaBuilder();
+
+        <T> MyTypedQuery<T> createQuery(MyCriteriaQuery<T> criteriaQuery);
+    }
+
+    private interface MyCriteriaBuilder {
+        <T> MyCriteriaQuery<T> createQuery(Class<T> clazz);
+
+        <E, C extends Collection<E>> Predicate isNotMember(E elem, MyExpression<C> collection);
+    }
+
+    private interface MyCriteriaQuery<T> {
+        MyRoot<T> from(Class<T> clazz);
+
+        MyCriteriaQuery<T> select(MyRoot<T> clazz);
+
+        MyCriteriaQuery<T> where(MyExpression<Boolean> restriction);
+    }
+
+    private interface MyRoot<X> {
+        //proposed change
+        <E, C extends Collection<E>> MyExpression<C> get(PluralAttribute<? super X, C, E> collection);
+        //original
+//        <E, C extends java.util.Collection<E>> Expression<C> get(PluralAttribute<X, C, E> collection);
+    }
+
+    private interface MySetAttribute<X, E> extends PluralAttribute<X, java.util.Set<E>, E> {
+    }
+
+    private interface MyExpression<C> {
+    }
+
+    private interface MyTypedQuery<X> {
+        List<X> getResultList();
+    }
 }
